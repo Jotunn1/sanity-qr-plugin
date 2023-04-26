81bd3b890662931775c514b1a1c80d411ede94f9
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-04-26 17:38:34 +0300

    added env variables for sanity project

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,14 +2,17 @@ import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
-import { myPlugin } from './plugins/qr-code-generator/src'
+import { myPlugin } from './plugins/qr-code-generator/src';
+
+const projectId: string | undefined = (process.env.SANITY_STUDIO_PROJECT_ID as string)
+const dataset: string | undefined = (process.env.SANITY_STUDIO_DATASET as string)
 
 export default defineConfig({
   name: 'default',
   title: 'sanity-test-project',
 
-  projectId: 'mz001lkk',
-  dataset: 'production',
+  projectId: projectId,
+  dataset: dataset,
 
   plugins: [deskTool(), visionTool(), myPlugin()],
 
